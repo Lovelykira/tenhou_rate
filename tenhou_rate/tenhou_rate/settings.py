bff94454209dbc0bb5df8a9410a2f8c1aac42982
Lovelykira <Lovelykira@gmail.com>
2016-11-13 20:45:30 +0200

    static dir

--- tenhou_rate/tenhou_rate/settings.py
+++ tenhou_rate/tenhou_rate/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
