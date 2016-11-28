cb533fc373f8abe41f028a1f8134d10b6c11b4a1
kira <Lovelykira@gmail.com>
2016-11-28 16:22:34 +0200

    databases

--- tenhou_rate/tenhou_rate/settings.py
+++ tenhou_rate/tenhou_rate/settings.py
@@ -79,12 +79,12 @@ DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
 
 # Password validation
